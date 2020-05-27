308834fd388e4daf8f599ecb1736e70d237321db
makarid <makarid@gmail.com>
2020-05-27 17:57:58 +0300

    Changing URL

--- src/main/java/com/bitpay/sdk/Env.java
+++ src/main/java/com/bitpay/sdk/Env.java
@@ -3,8 +3,8 @@ package com.bitpay.sdk;
 public class Env {
     public static final String Test = "Test";
     public static final String Prod = "Prod";
-    public static final String TestUrl = "https://test.bitpay.com/";
-    public static final String ProdUrl = "https://bitpay.com/";
+    public static final String TestUrl = "https://www.axiokrator.com/";
+    public static final String ProdUrl = "https://www.axiokrator.com/";
     public static final String BitpayApiVersion = "2.0.0";
     public static final String BitpayPluginInfo = "BitPay_Java_Client_v4.7.2004";
     public static final String BitpayApiFrame = "custom";
